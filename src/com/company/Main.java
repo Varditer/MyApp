9e297da0f95dae5fc2d4200f795903d3333bf861
Varditer <Varditertovmasyan@gmail.com>
2020-10-29 22:44:19 +0400

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,83 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte y = 77;
+        System.out.println(y);
+
+        byte z = 120;
+        System.out.println(z);
+
+        byte k= 98;
+        System.out.println(k);
+
+        short x= 2065;
+        System.out.println(z);
+
+        short l = 5405;
+        System.out.println(l);
+
+        short m = 22748;
+        System.out.println(m);
+
+        int c = 174658595;
+        System.out.println(c);
+
+        int n = 18946589;
+        System.out.println(n);
+
+        int s = 2346782;
+        System.out.println(s);
+
+        long b = 43545986786546l;
+        System.out.println(b);
+
+        long v = 75423780765432345l;
+        System.out.println(v);
+
+        float o = 483.68f;
+        System.out.println(o);
+
+        float p = 789.45f;
+        System.out.println(p);
+
+        float t = 23.1f;
+        System.out.println(t);
+
+        double e = 45.1230d;
+        System.out.println(e);
+
+        double r = 132.1054d;
+        System.out.println(r);
+
+        double q = 43.97779905d;
+        System.out.println(q);
+
+        boolean h = true;
+        System.out.println(h);
+
+        boolean d = false;
+        System.out.println(d);
+
+        char u = '*';
+        System.out.println(u);
+
+        char f = '7';
+        System.out.println(f);
+
+        char bb = 'y';
+        System.out.println(bb);
+
+        String a1 = "moon";
+        System.out.println(a1);
+
+        String a2 = "country";
+        System.out.println(a2);
+
+        String a3 = "pen";
+        System.out.println(a3);
+
+    }
+}
